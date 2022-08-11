33a67cddc766e8887152ea9a2ed4a2f4346a1e27
haris18896 <haris18896@gmail.com>
2022-08-11 21:27:56 +0500

    everything added

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+import HomeScreen from './screens/home/HomeScreen'
+import MapScreen from './screens/map-screen/MapScreen'
+import EatScreen from './screens/eat-screen/EatScreen'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name='Home'
+              component={HomeScreen}
+              options={{
+                presentation: 'fullScreenModal',
+                headerShown: false
+              }}
+            />
+            <Stack.Screen
+              name='MapScreen'
+              component={MapScreen}
+              options={{
+                presentation: 'fullScreenModal',
+                headerShown: false
+              }}
+            />
+            <Stack.Screen
+              name='EatScreen'
+              component={EatScreen}
+              options={{
+                presentation: 'fullScreenModal',
+                headerShown: false
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </Provider>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -37,4 +37,46 @@ export default GooglePlacesInput;
 # Google Places API KEY
 Go to the [Google Cloud Platform](https://cloud.google.com/) ===> click on `console` ===> Create a new project..
 
-After successfully Creating a project
+* After successfully Creating a project. before using the API's we have to enable Billing account,
+
+after that go to the `Dashboard of API's and Services` ==> click on `Enable API's and Services`
+
+1. the first one to enable is `directions api`
+2. secondly activate `places api`
+3.  Distance metrix api
+
+activate the above `API's`..
+
+Now the Second step is to set our credentials , so go to the `Credentials` to get a key. This will be a secret API key, copy it to some where safe, because this API KEY will access all the above API's. store it in `.env` file e.g
+
+```js
+// .env
+GOOGLE_MAPS_KEY=SuperSeCretkeYHere
+```
+
+to use `env` in react-native we have to install a package `react-native-dotenv`
+
+```
+npm install react-native-dotenv
+```
+
+and after that we have to add some settings to the `bable.config.js` file to support `env`
+
+```js
+// babel.config.js
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'nodule:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: './env'
+        }
+      ]
+    ]
+  }
+}
+```
